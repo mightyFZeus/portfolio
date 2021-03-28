fd0c73ca18a9e7993b13bb4d42aeceaa01e0c083
mightyFZeus <bolarinolabisi36@gmail.com>
2021-03-28 21:56:41 +0200

    mkl

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { Fragment } from 'react';
+
+function App() {
+  return (
+    <>
+      <h2>Welcome to the app component</h2>
+    </>
+  );
+}
+
+export default App;
